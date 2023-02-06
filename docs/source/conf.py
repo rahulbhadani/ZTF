0de72f87c66a204e9507a7e2077815adcf718b59
Rahul Bhadani <rahulbhadani@email.arizona.edu>
2023-02-06 00:47:37 -0600

    Classes for Z-transform and PID controller, documentation generation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,8 +18,10 @@
 # -- Project information -----------------------------------------------------
 
 project = 'ZTF'
-copyright = '2023, Rahul Bhadani'
+copyright = 'Rahul Bhadani'
 author = 'Rahul Bhadani'
+title = 'Digital Control using Z-transform'
+
 
 # The full version, including alpha/beta/rc tags
 release = '0.01'
@@ -31,6 +33,18 @@ release = '0.01'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.coverage",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.githubpages",
+    "sphinx_autodoc_typehints",
+    "nbsphinx",
+    "ytsphinx.youtube",
+    "m2r2",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -47,9 +61,84 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+html_theme_options = dict(navigation_depth=1, titles_only=True)
+github_repo = "ZTF"
+
+htmlhelp_basename = "ZTFdoc"
+title_doc = f"{project} documentation"
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+import sphinx_autodoc_typehints
+from typing import Dict, List, Tuple
+from docutils import nodes
+from sphinx import addnodes
+from sphinx.domains.python import PyTypedField, PyObject
+from sphinx.environment import BuildEnvironment
+
+class PrettyTypedField(PyTypedField):
+    list_type = nodes.definition_list
+
+    def make_field(
+        self,
+        types: Dict[str, List[nodes.Node]],
+        domain: str,
+        items: Tuple[str, List[nodes.inline]],
+        env: BuildEnvironment = None,
+    ) -> nodes.field:
+        def makerefs(rolename, name, node):
+            return self.make_xrefs(rolename, domain, name, node, env=env)
+
+        def handle_item(
+            fieldarg: str, content: List[nodes.inline]
+        ) -> nodes.definition_list_item:
+            head = nodes.term()
+            head += makerefs(self.rolename, fieldarg, addnodes.literal_strong)
+            fieldtype = types.pop(fieldarg, None)
+            if fieldtype is not None:
+                head += nodes.Text(" : ")
+                if len(fieldtype) == 1 and isinstance(fieldtype[0], nodes.Text):
+                    (text_node,) = fieldtype  # type: nodes.Text
+                    head += makerefs(
+                        self.typerolename, text_node.astext(), addnodes.literal_emphasis
+                    )
+                else:
+                    head += fieldtype
+
+            body_content = nodes.paragraph("", "", *content)
+            body = nodes.definition("", body_content)
+
+            return nodes.definition_list_item("", head, body)
+
+        fieldname = nodes.field_name("", self.label)
+        if len(items) == 1 and self.can_collapse:
+            fieldarg, content = items[0]
+            bodynode = handle_item(fieldarg, content)
+        else:
+            bodynode = self.list_type()
+            for fieldarg, content in items:
+                bodynode += handle_item(fieldarg, content)
+        fieldbody = nodes.field_body("", bodynode)
+        return nodes.field("", fieldname, fieldbody)
+
+
+# replace matching field types with ours
+PyObject.doc_field_types = [
+    PrettyTypedField(
+        ft.name,
+        names=ft.names,
+        typenames=ft.typenames,
+        label=ft.label,
+        rolename=ft.rolename,
+        typerolename=ft.typerolename,
+        can_collapse=ft.can_collapse,
+    )
+    if isinstance(ft, PyTypedField)
+    else ft
+    for ft in PyObject.doc_field_types
+]
